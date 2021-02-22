0fea9156839959c3427824b97d7436d31b6c1d9f
Kelsey Sparkman <kelseysparkman14@gmail.com>
2021-02-22 11:19:01 -0700

    create coreDataStack

--- MedicationManager/MedicationManager/CoreData/CoreDataStack.swift
+++ MedicationManager/MedicationManager/CoreData/CoreDataStack.swift
@@ -7,4 +7,27 @@
 
 import CoreData
 
-
+enum CoreDataStack {
+    
+    static let container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "MedicationManager")
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores \(error)")
+            }
+        }
+        return container
+    }()
+    
+    static var context: NSManagedObjectContext { container.viewContext }
+    
+    static func saveContext() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                print("Error saving context: \(error.localizedDescription)")
+            }
+        }
+    }
+}
