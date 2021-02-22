3be231c6dc37d5be1b50ebdd17c951e513a109cf
Kelsey Sparkman <kelseysparkman14@gmail.com>
2021-02-22 11:33:25 -0700

    add convenience init for medication

--- MedicationManager/MedicationManager/CoreData/Medication+Convenience.swift
+++ MedicationManager/MedicationManager/CoreData/Medication+Convenience.swift
@@ -7,4 +7,10 @@
 
 import CoreData
 
-
+extension Medication {
+    @discardableResult convenience init (name: String, timeOfDay: Date, context: NSManagedObjectContext = CoreDataStack.context) {
+        self.init(context: context)
+        self.name = name
+        self.timeOfDay = timeOfDay
+    }
+}
